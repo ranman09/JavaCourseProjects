76518ccd7d3e35aa5c7505bdc6a2a8af9919774e
ranman09 <28801036+ranman09@users.noreply.github.com>
2019-06-18 04:04:10 +0800

    Min element challenge first solution

---
+++ MinElementChallenge/src/com/armo/Main.java
@@ -0,0 +1,35 @@
+package com.armo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int [] array = readIntegers(5);
+        System.out.println(Arrays.toString(array));
+        System.out.println("Minimum value is : " + findMin(array));
+    }
+
+    public static int [] readIntegers(int numbers){
+        int [] values = new int[numbers];
+        System.out.println("Enter " + numbers + " numbers: ");
+        for (int i = 0; i < numbers; i++){
+            values[i] = scanner.nextInt();
+        }
+        scanner.close();
+        return values;
+    }
+
+    public static int findMin(int [] array){
+        int minVal = array[0];
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < minVal){
+                minVal = array[i];
+            }
+        }
+        return minVal;
+    }
+}
